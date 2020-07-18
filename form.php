c43767d1e7a54f01b0751f78bc685cfccbd39757
loko prudencia <prudencialoko229@gmail.com>
2020-07-18 15:31:38 +0100

    envoie de fichier

---
+++ form.php
@@ -0,0 +1,22 @@
+﻿ 
+ <?php
+        
+    try
+        {
+            
+           $connexion = new PDO('mysql:host=localhost; dbname=boutique', 'root','');
+        }
+            
+    catch(Exception $e)
+    {
+            
+       die('Erreur :'.$e->getMessage());
+    }
+    $pdoconnect = $connexion->prepare('INSERT INTO formulaire(nom, prenom,mel,command) VALUES ( :nom, :prenom, :mel, :command)');     
+    $pdoconnect->execute(array('nom'=>$_POST['seudo'],'prenom'=>$_POST['seudo'], 'mel'=>$_POST['mail'], 'command'=>$_POST['commande']));
+
+    
+            $message = 'Votre commande a été bien enregistrer. ';
+    
+
+?>
